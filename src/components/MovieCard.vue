4c3e8b28c68a3550c55f69454172fde5eeb971e2
Matteo Tagliatti <matteotagliatti@gmail.com>
2022-04-01 14:00:28 +0200

    add stelline to movies

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -8,7 +8,7 @@
       <li>{{ movieData.title }}</li>
       <li>{{ movieData.original_title }}</li>
       <li><span :class="selectFlag()"></span></li>
-      <li>{{ movieData.vote_average }}</li>
+      <li>{{ selectVote() }}</li>
     </ul>
   </div>
 </template>
@@ -26,6 +26,14 @@ export default {
       } else {
         return "fi fi-xx";
       }
+    },
+    selectVote() {
+      const vote = this.movieData.vote_average;
+      if (vote < 2) return "★";
+      if (vote > 2 && vote < 4) return "★★";
+      if (vote > 4 && vote < 6) return "★★★";
+      if (vote > 6 && vote < 8) return "★★★★";
+      if (vote > 8 && vote < 10) return "★★★★★";
     },
   },
 };
